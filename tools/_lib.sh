fc8793bf94b922d4161f47c17d98de1d9060b6cb
Brian W. Wolter <brianwolter@gmail.com>
2022-04-23 19:20:06 -0400

    ...

--- tools/_lib.sh
+++ tools/_lib.sh
@@ -2,3 +2,9 @@
 if [ -z "$ENVIRON" ]; then
   ENVIRON=development
 fi
+if [ -z "$GITHASH" ]; then
+  GITHASH=$(git log --pretty=format:'%h' -n 1)
+fi
+if [ -z "$VERSION" ]; then
+  VERSION=$GITHASH
+fi
