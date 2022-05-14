6c25b82b88ab71f852ac41f772fb37f8b53c8ccb
Brian W. Wolter <brian@brianwolter.com>
2022-05-14 16:47:06 -0400

    ...

--- src/update/mod.rs
+++ src/update/mod.rs
@@ -22,44 +22,74 @@ use std::path;
 //   }
 // }
 
-#[derive(Debug, Clone)]
-pub struct Version<R: io::Read> {
-  version: usize,
-  descr: String,
-  resource: R,
+pub trait IntoRead<R: io::Read> {
+  fn into_read(&self) -> Result<R, error::Error>;
 }
 
-impl<R: io::Read> Version<R> {
-  pub fn new(version: usize, descr: String, resource: R) -> Version<R> {
+pub struct FileIntoRead {
+  path: path::PathBuf,
+}
+
+impl IntoRead<fs::File> for FileIntoRead {
+  fn into_read(&self) -> Result<fs::File, error::Error> {
+    Ok(fs::File::open(&self.path)?)
+  }
+}
+
+#[derive(Debug, Clone)]
+pub struct Version<T: io::Read, R: IntoRead<T>> {
+  version: usize,
+  descr: String,
+  reader: R,
+  dummy: Option<T>,
+}
+
+impl<T: io::Read, R: IntoRead<T>> Version<T, R> {
+  pub fn new(version: usize, descr: String, reader: R) -> Version<T, R> {
     Version{
       version: version,
       descr: descr,
-      resource: resource,
+      reader: reader,
+      dummy: None,
     }
   }
 }
 
-impl<R: io::Read> cmp::PartialOrd for Version<R> {
+impl<T: io::Read, R: IntoRead<T>> IntoRead<T> for Version<T, R> {
+  fn into_read(&self) -> Result<T, error::Error> {
+    self.reader.into_read()
+  }
+}
+
+/*
+impl<R: io::Read> io::Read for Version<R> {
+  fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+    self.resource.read(buf)
+  }
+}
+*/
+
+impl<T: io::Read, R: IntoRead<T>> cmp::PartialOrd for Version<T, R> {
   fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
     self.version.partial_cmp(&other.version)
   }
 }
 
-impl<R: io::Read> cmp::Ord for Version<R> {
+impl<T: io::Read, R: IntoRead<T>> cmp::Ord for Version<T, R> {
   fn cmp(&self, other: &Self) -> cmp::Ordering {
     self.version.cmp(&other.version)
   }
 }
 
-impl<R: io::Read> cmp::Eq for Version<R> {}
-impl<R: io::Read> cmp::PartialEq for Version<R> {
+impl<T: io::Read, R: IntoRead<T>> cmp::Eq for Version<T, R> {}
+impl<T: io::Read, R: IntoRead<T>> cmp::PartialEq for Version<T, R> {
   fn eq(&self, other: &Self) -> bool {
     self.version == other.version
   }
 }
 
-impl Version<fs::File> {
-  pub fn from_entry(entry: fs::DirEntry) -> Result<Option<Version<fs::File>>, error::Error> {
+impl Version<fs::File, FileIntoRead> {
+  pub fn from_entry(entry: fs::DirEntry) -> Result<Option<Version<fs::File, FileIntoRead>>, error::Error> {
     let m = entry.metadata()?;
     if !m.is_file() {
       return Ok(None);
@@ -106,7 +136,10 @@ impl Version<fs::File> {
       Some(Version{
         version: v,
         descr: name.to_string(),
-        resource: fs::File::open(path)?,
+        reader: FileIntoRead{
+          path: path::PathBuf::from(path),
+        },
+        dummy: None,
       })
     )
   }
@@ -129,7 +162,7 @@ impl DirectoryProvider {
 }
 
 impl iter::Iterator for DirectoryProvider {
-  type Item = Result<Version<fs::File>, error::Error>;
+  type Item = Result<Version<fs::File, FileIntoRead>, error::Error>;
   fn next(&mut self) -> Option<Self::Item> {
     loop {
       if let Some(e) = self.iter.next() {
@@ -156,17 +189,24 @@ mod tests {
   #[test]
   fn provide_versions() {
     let iter = DirectoryProvider::new_with_path("./etc/db").unwrap();
-    let mut versions: Vec<Version<fs::File>> = Vec::new();
+    
+    let mut versions: Vec<Version<fs::File, IntoRead<fs::File>>> = Vec::new();
     for v in iter {
       match v {
         Ok(v) => versions.push(v),
         Err(err) => panic!("*** {}", err),
       };
     }
+    
     versions.sort();
-    for v in versions {
+    for mut v in versions {
       println!("### {:?}", v);
+      match io::copy(&mut v.into_read()?, &mut io::stdout()) {
+        Ok(n) => println!(">>> {} bytes", n),
+        Err(err) => panic!("*** {}", err),
+      };
     }
+    
   }
   
 }
