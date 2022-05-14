eb1a04f5f679e3544b6efd3d4cf44fde16ad2f41
Brian W. Wolter <brian@brianwolter.com>
2022-05-14 15:06:09 -0400

    ...

--- src/update/error.rs
+++ src/update/error.rs
@@ -1,10 +1,12 @@
 use std::io;
 use std::fmt;
+use std::num;
 
 #[derive(Debug)]
 pub enum Error {
   DriverError,
   IOError(io::Error),
+  ParseIntError(num::ParseIntError),
 }
 
 impl From<io::Error> for Error {
@@ -13,11 +15,18 @@ impl From<io::Error> for Error {
   }
 }
 
+impl From<num::ParseIntError> for Error {
+  fn from(error: num::ParseIntError) -> Self {
+    Self::ParseIntError(error)
+  }
+}
+
 impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
       Self::DriverError => "driver_error".fmt(f),
       Self::IOError(err) => err.fmt(f),
+      Self::ParseIntError(err) => err.fmt(f),
     }
   }
 }
--- src/update/mod.rs
+++ src/update/mod.rs
@@ -2,6 +2,7 @@ pub mod error;
 
 use std::io;
 use std::fs;
+use std::cmp;
 use std::iter;
 use std::path;
 
@@ -21,11 +22,97 @@ use std::path;
 //   }
 // }
 
-// pub trait Version: io::Read {
-//   fn version(&self) -> usize;
-// }
+#[derive(Debug, Clone)]
+pub struct Version<R: io::Read> {
+  version: usize,
+  descr: String,
+  resource: R,
+}
 
-pub trait VersionProvider: iter::IntoIterator {}
+impl<R: io::Read> Version<R> {
+  pub fn new(version: usize, descr: String, resource: R) -> Version<R> {
+    Version{
+      version: version,
+      descr: descr,
+      resource: resource,
+    }
+  }
+}
+
+impl<R: io::Read> cmp::PartialOrd for Version<R> {
+  fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+    self.version.partial_cmp(&other.version)
+  }
+}
+
+impl<R: io::Read> cmp::Ord for Version<R> {
+  fn cmp(&self, other: &Self) -> cmp::Ordering {
+    self.version.cmp(&other.version)
+  }
+}
+
+impl<R: io::Read> cmp::Eq for Version<R> {}
+impl<R: io::Read> cmp::PartialEq for Version<R> {
+  fn eq(&self, other: &Self) -> bool {
+    self.version == other.version
+  }
+}
+
+impl Version<fs::File> {
+  pub fn from_entry(entry: fs::DirEntry) -> Result<Option<Version<fs::File>>, error::Error> {
+    let m = entry.metadata()?;
+    if !m.is_file() {
+      return Ok(None);
+    }
+    
+    let path = entry.path();
+    let ext = match path.extension() {
+      Some(ext) => ext,
+      None => return Ok(None),
+    };
+    if let Some(ext) = ext.to_str() {
+      if ext != "sql" {
+        return Ok(None);
+      }
+    }
+    
+    let name = match path.file_name() {
+      Some(name) => name,
+      None => return Ok(None),
+    };
+    let name = match name.to_str() {
+      Some(name) => name,
+      None => return Ok(None),
+    };
+    let chars = name.chars();
+    let mut buf = String::new();
+    for c in chars {
+      if char::is_numeric(c) {
+        buf.push(c);
+      }else{
+        break;
+      }
+    }
+    if buf.len() == 0 {
+      return Ok(None)
+    }
+    
+    let v = match buf.parse::<usize>() {
+      Ok(v) => v,
+      Err(err) => return Err(err.into()),
+    };
+    
+    Ok(
+      Some(Version{
+        version: v,
+        descr: name.to_string(),
+        resource: fs::File::open(path)?,
+      })
+    )
+  }
+}
+
+pub trait VersionProvider: iter::Iterator {}
 
 pub struct DirectoryProvider {
   dir: path::PathBuf,
@@ -42,15 +129,22 @@ impl DirectoryProvider {
 }
 
 impl iter::Iterator for DirectoryProvider {
-  type Item = Result<path::PathBuf, error::Error>;
+  type Item = Result<Version<fs::File>, error::Error>;
   fn next(&mut self) -> Option<Self::Item> {
-    if let Some(e) = self.iter.next() {
-      match e {
-        Ok(e) => Some(Ok(path::PathBuf::from(e.path()))),
-        Err(err) => Some(Err(err.into())),
+    loop {
+      if let Some(e) = self.iter.next() {
+        match e {
+          Ok(e) => match Version::from_entry(e) {
+            Ok(v) => if let Some(v) = v {
+              return Some(Ok(v));
+            },
+            Err(err) => return Some(Err(err.into())),
+          },
+          Err(err) => return Some(Err(err.into())),
+        }
+      }else{
+        return None;
       }
-    }else{
-      None
     }
   }
 }
@@ -61,9 +155,17 @@ mod tests {
   
   #[test]
   fn provide_versions() {
-    let p = DirectoryProvider::new_with_path(".").unwrap();
-    for v in p {
-      println!(">>> {:?}", v);
+    let iter = DirectoryProvider::new_with_path("./etc/db").unwrap();
+    let mut versions: Vec<Version<fs::File>> = Vec::new();
+    for v in iter {
+      match v {
+        Ok(v) => versions.push(v),
+        Err(err) => panic!("*** {}", err),
+      };
+    }
+    versions.sort();
+    for v in versions {
+      println!("### {:?}", v);
     }
   }
   
