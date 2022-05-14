75e6148c78eb548b7346273a9aa0a60fa2383d5b
Brian W. Wolter <brian@brianwolter.com>
2022-05-14 10:15:00 -0400

    ...

--- src/update/mod.rs
+++ src/update/mod.rs
@@ -5,49 +5,53 @@ use std::fs;
 use std::iter;
 use std::path;
 
-pub trait Driver {
-  fn version(&self) -> Result<usize, error::Error>;
+// pub trait Driver {
+//   fn version(&self) -> Result<usize, error::Error>;
+// }
+
+// pub struct Updater<D: Driver> {
+//   driver: D,
+// }
+
+// impl<D> Updater<D> {
+//   pub fn new(driver: D) -> Self {
+//     Self{
+//       driver: driver,
+//     }
+//   }
+// }
+
+// pub trait Version: io::Read {
+//   fn version(&self) -> usize;
+// }
+
+pub trait VersionProvider: iter::IntoIterator {}
+
+pub struct DirectoryProvider {
+  dir: path::PathBuf,
+  iter: fs::ReadDir,
 }
 
-pub struct Updater<D: Driver> {
-  driver: D,
-}
-
-impl<D> Updater<D> {
-  pub fn new(driver: D) -> Self {
-    Self{
-      driver: driver,
-    }
-  }
-}
-
-pub trait Version: io::Read {
-  fn version(&self) -> usize;
-}
-
-pub trait VersionProvider: IntoIterator {}
-
-pub struct DirectoryProvider<I: iter::Iterator<Item=io::Result<fs::DirEntry>>> {
-  dir: path::Path,
-  paths: I,
-}
-
-impl<I> DirectoryProvider<I> {
+impl DirectoryProvider {
   pub fn new_with_path<P: AsRef<path::Path>>(path: P) -> Result<Self, error::Error> {
-    Self{
-      dir: path.to_owned(),
-      paths: fs::read_dir(path)?,
-    }
+    Ok(Self{
+      dir: path::PathBuf::from(path.as_ref()),
+      iter: fs::read_dir(path)?,
+    })
   }
 }
 
-impl<I> IntoIterator for DirectoryProvider<I> {
-  type Item = Result<dyn Version, error::Error>;
-  type IntoIter = Result<dyn Version, error::Error>;
+impl iter::Iterator for DirectoryProvider {
+  type Item = Result<path::PathBuf, error::Error>;
   fn next(&mut self) -> Option<Self::Item> {
-    let path = self.paths.next()?;
-    println!(">>> {}", path);
-    None
+    if let Some(e) = self.iter.next() {
+      match e {
+        Ok(e) => Some(Ok(path::PathBuf::from(e.path()))),
+        Err(err) => Some(Err(err.into())),
+      }
+    }else{
+      None
+    }
   }
 }
 
@@ -57,7 +61,7 @@ mod tests {
   
   #[test]
   fn provide_versions() {
-    let p = DirectoryProvider::new_with_path().unwrap();
+    let p = DirectoryProvider::new_with_path(".").unwrap();
     for v in p {
       println!(">>> {:?}", v);
     }
