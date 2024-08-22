d295c192f903afa13a5ecc859eaa7d640a964b2b
Brian W. Wolter <brianwolter@gmail.com>
2024-08-22 10:25:04 -0400

    ...

--- tools/_lib.sh
+++ tools/_lib.sh
@@ -1,23 +1,19 @@
 
-if [ -z "$ENVIRON" ]; then
-  echo "\$ENVIRON is not defined"
-  exit 1
-fi
-
-if [ -z "$GITHASH" ]; then
-  GITHASH=$(git log --pretty=format:'%h' -n 1)
-fi
-if [ -z "$VERSION" ]; then
-  VERSION=$GITHASH
-fi
-if [ -z "$INSTAUNIT" ]; then
-  INSTAUNIT=instaunit
-fi
+GITHASH=${GITHASH:=$(git log --pretty=format:'%h' -n 1)}
+VERSION=${VERSION:=$GITHASH}
+INSTAUNIT=${INSTAUNIT:=instaunit}
 
 append_flags () {
   if [ -z "$1" ]; then
     echo "$2"
   else
     echo "$1 $2"
+  fi
+}
+
+assert_flag () {
+  if [ -z "$1" ]; then
+    echo "*** $2"
+    exit 1
   fi
 }
